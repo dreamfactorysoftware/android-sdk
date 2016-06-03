9bcf116dd5f6fbec242674aaed79588b0f9963d8
Nirmel <nirmel.murtic@gmail.com>
2016-06-03 13:39:26 +0200

    - My configuration

--- android-sdk/app/src/main/java/com/dreamfactory/sampleapp/utils/AppConstants.java
+++ android-sdk/app/src/main/java/com/dreamfactory/sampleapp/utils/AppConstants.java
@@ -3,8 +3,8 @@ package com.dreamfactory.sampleapp.utils;
 public interface AppConstants {
     String SESSION_TOKEN = "session_token";
     // Note: emulator IP for host's localhost is 10.0.2.2
-    String INSTANCE_URL = "http://10.0.2.2:8080/api/v2";
+    String INSTANCE_URL = "https://df-ft-nirmel-murtic452.enterprise.dreamfactory.com/api/v2";
     String DB_SVC = "db/_table";
     // API key for your app goes here, see apps tab in admin console
-    String API_KEY = "55d7ad6d21915e8539b7939f8604b4c096eff005bc2d431abd9b427a45d55e22";
+    String API_KEY = "405b6f4da730ca78933654842be162024911a4250dc8e67a615e342dd2903eaf";
 }
